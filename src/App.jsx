24e766a43a7e19c1ba71eb41ca528688a450a5c1
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2025-05-29 19:26:27 +0300

    add property

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,18 @@ function App() {
       <div>
         <p>hello world</p>
       </div>
-      <Text />
-      <Text />
-      <Text />
-      <Text />
-      <Text />
+      <Text display="hello world" />
+      <Text display="yes" />
+      <Text display="no" />
+      <Text display="oops" />
     </>
   );
 }
 
-function Text() {
+function Text({ display }) {
   return (
     <div>
-      <p>hello world</p>
+      <p>{display}</p>
     </div>
   );
 }
