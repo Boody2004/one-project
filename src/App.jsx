15340fd65a9140efb94f672efd633225eac9673a
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2025-05-29 21:23:12 +0300

    simple conditional render

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import "./App.css";
 import MovieCard from "./components/MovieCard";
 
 function App() {
+  const movieNumber = 2;
+
   return (
     <>
-      <MovieCard movie={{ title: "title", release_data: "20.20.2025" }} />
+      {movieNumber === 1 ? (
+        <MovieCard movie={{ title: "title", release_data: "20.20.2025" }} />
+      ) : (
+        <MovieCard movie={{ title: "title2", release_data: "22.22.2025" }} />
+      )}
     </>
   );
 }
