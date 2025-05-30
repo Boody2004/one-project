9f75dee0fdba90547ad1370cc73bf17d483d1582
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2025-05-30 05:33:33 +0300

    add search using usestate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import MovieCard from "../components/MovieCard";
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     { id: 1, title: "title1", release_data: "2025" },
     { id: 2, title: "title2", release_data: "2025" },
@@ -8,12 +11,34 @@ function Home() {
     { id: 4, title: "title4", release_data: "2025" },
   ];
 
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("");
+  };
+
   return (
     <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movie..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+
       <div className="movie-grid">
-        {movies.map((movie) => (
-          <MovieCard movie={movie} key={movie.id} />
-        ))}
+        {movies.map(
+          (movie) =>
+            movie.title.toLowerCase().startsWith(searchQuery) && (
+              <MovieCard movie={movie} key={movie.id} />
+            )
+        )}
       </div>
     </div>
   );
